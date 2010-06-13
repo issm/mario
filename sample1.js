f57c97e07d3163c881cf1b6ed37d8c48f7fc5b6b
issm <issmxx@gmail.com>
2010-06-13 18:14:27 +0900

    fixed.

--- sample1.js
+++ sample1.js
@@ -1,87 +1,38 @@
 window.onload = function() {
 
 (function(){
-for( var i = 0; i < 1; i++ ) {
-  setTimeout( function() {
+for (var i = 0; i < 1; i++) {
+    setTimeout(function () {
+        var Mario_or_Luigi = (parseInt(Math.random() * 100) % 2) ? Mario : Luigi;
+        var status = (parseInt(Math.random() * 100) % 2) ? 'SUPER' : 'MINI';
 
-  var y = 240 + parseInt( ( Math.random() - .5 ) * 240 );
+        var x = Math.random() * 600;
+        var y = 240 + parseInt((Math.random() - .5) * 240);
 
-  var mario = new Mario({
-    x:           Math.random() * 400,
-    y:           y,
-    scale: 2,
-    ability: {
-      accel: .25,
-      bdash: 2,
-      jump:  6
-    },
-    environment: new Mario.Environment({
-      gravity: 9.8
-    })
-    ,status: 'SUPER'
-  });
+        var mario = new Mario_or_Luigi({
+            x:     x,
+            y:     y,
+            scale: 2,
+            ability: {
+                accel: .25,
+                bdash: 2,
+                jump:  6
+            },
+            environment: new Mario.Environment({
+                gravity: 9.8
+            })
+            ,status: status
+        });
 
-  var luigi = new Luigi({
-    x:           Math.random() * 400,
-    y:           y,
-    scale: 2,
-    ability: {
-      accel: .15 + ( Math.random() - .5 ) * .025,
-      bdash: 2.5 + ( Math.random() - .5 ) * 1,
-      jump:  7   + ( Math.random() - .5 ) * 2
-    },
-    environment: new Mario.Environment({
-      gravity: 8.8
-    })
-    //,status: 'SUPER'
-  });
+        var controller_mario = new Mario.Controller({
+            target: mario
+        });
 
-  var controller_mario = new Mario.Controller({
-    mario: mario
-  });
-  var controller_luigi = new Mario.Controller({
-    mario: luigi
-  });
+        setTimeout(function() {
+            mario.JUMP();
+        }, 0);
 
-  setTimeout( function() {
-    mario.JUMP();
-    luigi.JUMP();
-  }, 0 );
-
-  }, 0 );
-
-
-/*
-  window.mario2 = new Mario({
-    x: 200,
-    y: 200,
-    ability: {
-      accel: .5
-
-    }
-  });
-  mario2.WALK();
-
-  var toggle = false;
-  var timer = setInterval( function() {
-    if( ! toggle  &&  mario2.__X >= 600 ) {
-      toggle = true;
-      mario2.update_velocity( -1 );
-      //clearInterval( timer );
-    }
-    else if( toggle  &&  mario2.__X <= 100 ) {
-      toggle = false;
-      mario2.update_velocity( 1 );
-    }
-    else {
-      mario2.update_velocity( toggle ? -1 : 1 );
-    }
-  }, 20 );
-
-*/
-
-
-
+  }, 0);
 }
 
 
